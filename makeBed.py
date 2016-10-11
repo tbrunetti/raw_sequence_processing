a0a9a829795c6b2f8d6f4ecfd96cb2adb701d1eb
tbrunetti <tonya.brunetti@colorado.edu>
2016-10-11 11:34:23 -0500

    made so keeps log files of reads not used

--- makeBed.py
+++ makeBed.py
@@ -9,10 +9,13 @@ def main():
 			temp.append(data)
 
 	f=open(str(sys.argv[1][:len(sys.argv[1])-6])+'.bed', 'w')
-
+	f2=open(str(sys.argv[1][:len(sys.argv[1])-6])+'.bed.log', 'w')
+	f2.write('ERROR! Following sequences not formatted properly:'+'\n')
 	for i in range(0, len(temp)):
-		if str(temp[i][0]) != '.':
+		if str(temp[i][0]) != '.' and float(temp[i][1])<float(temp[i][5]):
 			f.write(str(temp[i][0])+'\t'+str(temp[i][1]+'\t'+str(temp[i][5]))+'\n')
+		else:
+			f2.write('\t'.join(temp[i])+'\n')
 
 
 if __name__=='__main__':
